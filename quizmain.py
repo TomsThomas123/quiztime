4a1d0e0eab9b0879d8621a8c8e0c16baf50f8378
TomsThomas123 <137117221+TomsThomas123@users.noreply.github.com>
2023-08-10 19:21:03 -0500

    Update quizmain.py

--- quizmain.py
+++ quizmain.py
@@ -4,30 +4,31 @@ if playing != 'yes':
     quit
 print("Okay! Let's play!")
 score = 0
-a1 = input('what does GPU stand for? ')
+a1 = input('What does GPU stand for? ')
 if a1 == "graphics processing unit":
     print('Correct!')
     score = score + 1
 else:
     print('Incorrect!')
-a1 = input('what does GPlU stand for? ')
-if a1 == "graphics processing unit":
+a1 = input('Who is the most popular Youtuber? ')
+if a1 == "MrBeast":
     print('Correct!')
     score = score + 1
 else:
     print('Incorrect!')
 
-a1 = input('what does GwPU stand for? ')
-if a1.lower == "graphics processing unit":
+a1 = input('What is the fastest AMD CPU.? ')
+if a1 == "Ryzen 9 7950X3D":
     print('Correct!')
     score = score + 1
 else:
     print('Incorrect!')
 
-a1 = input('what does GPvU stand for? ')
-if a1.lower == "graphics processing unit":
+a1 = input('What is the worlds most dangerous toy? ')
+if a1. == "Hasbro Javelin Darts:
     print('Correct!')
     score = score + 1
 else:
     print('Incorrect!')
+
 print('You got ' + str(score) + "questions correct!")
